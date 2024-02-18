db8d2e17a2a7385c3d2a9c97f7307d7d92fa7827
dkasy64 <dawitkasy64@gmail.com>
2024-02-18 18:34:38 -0500

    snack prices and numbers assigned and first case written

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class VendingMachine{
@@ -19,12 +20,26 @@ decides to cancel the transaction, they are given a full refund.
         System.out.println("Hello! Welcome to our vending machine.");
         System.out.println("You can choose one snack from a selection of different snacks.");
         // will have to include actual names of snacks
-        System.out.println("Snacks that we have available: \nxxxx: $$$\nxxxx: $$$\nxxxx: $$$");
+        System.out.println("Snacks that we have available: \n1. Doritos: $2.00\n2. Snickers: $1.50\n3. Pepsi: $4.00\n4. Lemonade: $3.00\n5. Skittles: $3.50");
+        double doritos = 2.00;
+        double snickers = 1.50;
+        double pepsi = 4.00;
+        double lemonade = 3.00;
+        double skittles = 3.50;
+        double change = 0.00;
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the amount of money you would like to insert into the machine. We only accept full dollar amounts.");
-        int money = sc.nextInt();
-
+        int money = sc.nextInt(); 
+        System.out.println("Please enter what snack you would like based on the associated number");
+        int item = sc.nextInt();
+        switch(item){
+            case 1:
+            System.out.println("You chose Doritos");
+            change = money - doritos;
+            System.out.println("Your change is:\n$" + change);
+        }
 
         sc.close();
+        
     }
 }
